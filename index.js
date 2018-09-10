4144da6a831b8f838c6a9026af7546587e5bb047
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 22:46:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,19 @@ class BoardMember {
         this.training = training;
     }
 
+    veto() {
+        return `No, I must disagree`;
+    }
+
+    approve() {
+        return `You can do that!`;
+    }
+
+    doCharity() {
+        return `I like to help people.`;
+    }
+
+    releasePressStatement() {
+        return `You will see great things from Scuber.`;
+    }
 }
