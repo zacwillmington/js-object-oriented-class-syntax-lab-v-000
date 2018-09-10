131fc91ca299f6e8f2a829bcb99a367d5f9c6209
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 22:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,4 +20,8 @@ class BoardMember {
     releasePressStatement() {
         return `You will see great things from Scuber.`;
     }
+
+    sayHi() {
+         return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    }
 }
