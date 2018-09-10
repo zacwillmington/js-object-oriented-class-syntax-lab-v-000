ac6b0d529f267f0163c17bf8ea48fc2dceca87a5
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 22:44:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function BoardMember (name, homeState, training) {
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
+class BoardMember {
+    constructor (name, homeState, training) {
+        this.name = name;
+        this.homeState = homeState;
+        this.training = training;
+    }
+
 }
